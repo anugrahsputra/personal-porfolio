62f293f2ba5c3deba837addfdace7c34bafab54b
anugrahsputra <anugrahsputra@gmail.com>
2025-09-26 20:48:01 +0700

    add seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Its Your Boy Putra",
   description: "A professional portfolio showcasing my work and skills",
+  verification: {
+    google: "UNzsS_OM9oAp-ntWRGrJXh7XUCQh-hRlSBZkjk3IsyI",
+  },
 };
 
 export default function RootLayout({
